c02dc6501035f4d526f01267cca63544906d5e43
0RisHI <rishi6767071@gmail.com>
2024-11-17 17:43:34 +0530

    Update problem 8.java

--- problem 8.java
+++ problem 8.java
@@ -1,29 +1,16 @@
-/* Write a program that takes an integer input n and prints an inverted triangle of numbers.
- * For example, if n = 5, the output should be:
- */
-
-
 import java.util.Scanner;
 
-public class prob8 {
+public class InvertedTriangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
-        int n = sc.nextInt();
-        printInvPyramid(n);
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    public static void printInvPyramid(int n) {
-        int i, j;
-        // outer loop to handle number of rows
-        for (i = n; i >= 1; i--) {
-            // inner loop to handle number of columns
-            for (j = 1; j <= i; j++) {
-                // printing column values upto the row value
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
-
-            // print new line for each row
             System.out.println();
         }
     }
